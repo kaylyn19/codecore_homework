759753430c165ccd90e9cadd30dc56f975975121
Kaylyn Kwak <kkwak0803@gmail.com>
2019-06-17 22:33:12 -0700

    complete complete functionality

--- Todo_CLI/todo_CLI.js
+++ Todo_CLI/todo_CLI.js
@@ -13,13 +13,12 @@ const getData =function (file_name) {
         }
     })
 }
-// const add = function (response) {
-//     fs.writeFile(file, response, (err) => {
-//         if (err) {
-//             console.err(err);
-//         } return;
-//     })
-// }
+
+const write = function(file_name, output) {
+    fs.writeFile(fileName, output, (err) => {
+        if (err) {console.err(err)} 
+    })
+}
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -29,6 +28,7 @@ const rl = readline.createInterface({
 let list = '';
 const regex = /[ ]/g
 const fileName = 'todo_list.txt';
+const splitTaskInArr = fs.readFileSync(fileName).toString().split('\n');
 console.log('Welcome to TODO CLI!\n------------------\n')
 
 const menu = () => {
@@ -38,10 +38,10 @@ const menu = () => {
         } else if (response.toLowerCase() === 'n') {
             rl.question('what?\n>', (addNew) => {
                 let order = 1;
-                if (fs.readFileSync(fileName).length === 0) {
+                if (splitTaskInArr.length === 0) {
                     order = 1;
                 }else {
-                    order = fs.readFileSync(fileName).toString().split('\n').length;
+                    order = splitTaskInArr.length;
                 }
                 list += `${order} [ ] ${addNew}\n`
                 fs.appendFile(fileName, list, {encoding:'utf8'}, (err) => {
@@ -53,11 +53,36 @@ const menu = () => {
                 })
             })
         } else if (response.toLowerCase()[0] === 'c') {
-            for (let tasks of fs.readFileSync(fileName).toString().split('\n')) {
-                const indexOfSpace = tasks.indexOf(tasks.match(regex)[0]);
-            }
+            completeResult = '';
+            for (let tasks of splitTaskInArr) {
+                let taskSplit = tasks.split(" ");
+                if (taskSplit[0] === response.slice(1)) {
+                    let indexOfSelected = splitTaskInArr.indexOf(tasks)
+                    completeResult += `${taskSplit[0]} [*] ${taskSplit[3]}\n`;
+                    splitTaskInArr.splice(indexOfSelected, 1, completeResult);
+                   console.log(`\ncompleted ${taskSplit[3]}\n`)
+                } else {
+                    completeResult += `${tasks}\n`
+                } 
+            } 
+            write(fileName, completeResult);
+            menu();
         } else if (response.toLowerCase()[0] === 'd') {
-
+            deleteResult = '';
+            for (let tasks of splitTaskInArr) {
+                let taskSplit = tasks.split(" ");
+                if (taskSplit[0] === response.slice(1)) {
+                    let indexOfSelected = splitTaskInArr.indexOf(tasks)
+                    console.log(indexOfSelected)
+                    // deleteResult += `${taskSplit[0]} [*] ${taskSplit[3]}\n`;
+                    splitTaskInArr.splice(indexOfSelected, 1);
+                    console.log(`\ncompleted ${taskSplit[3]}\n`)
+                } else {
+                    deleteResult += `${tasks}\n`
+                }
+            } console.log(deleteResult)
+            // write(fileName, deleteResult);
+            menu();
         } else {
             rl.close();
         }
