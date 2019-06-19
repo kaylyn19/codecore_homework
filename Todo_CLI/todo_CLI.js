9df3569e90c0853f3efc45203b5a79c5814a0323
Kaylyn Kwak <kkwak0803@gmail.com>
2019-06-18 19:23:01 -0700

    final draft before confirmation

--- Todo_CLI/todo_CLI.js
+++ Todo_CLI/todo_CLI.js
@@ -15,7 +15,7 @@ const getData =function (file_name) {
 }
 
 const write = function(file_name, output) {
-    fs.writeFile(fileName, output, (err) => {
+    fs.writeFile(file_name, output, (err) => {
         if (err) {console.err(err)} 
     })
 }
@@ -25,29 +25,28 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-// let list = '';
-// const fileName = 'todo_list.txt';
-// const splitTaskInArr = fs.readFileSync(fileName).toString().split('\n');
+const fileName = 'todo_list.txt';
+// const splitTaskArr = () => fs.readFileSync(fileName).toString().split('\n');
 console.log('Welcome to TODO CLI!\n------------------\n')
 
 const menu = () => {
     rl.question ('(v) View • (n) New • (cX) Complete • (dX) Delete • (q) Quit\n>', (response) => {
         if (response.toLowerCase() === 'v') {
-            getData('todo_list.txt');
+            getData(fileName);
         }
         
         else if (response.toLowerCase() === 'n') {
             rl.question('what?\n>', (addNew) => {
                 let list = '';
                 let countTasksInFile = 1;
-                if (fs.readFileSync('todo_list.txt').toString().split('\n').length === 0) {
+                if (fs.readFileSync(fileName).toString().split('\n').length === 0) {
                     countTasksInFile = 1;
                 } else {
-                    countTasksInFile = fs.readFileSync('todo_list.txt').toString().split('\n').length - 1;
+                    countTasksInFile = fs.readFileSync(fileName).toString().split('\n').length - 1;
                 }
 
                 list += `${countTasksInFile} [ ] ${addNew}\n`;
-                fs.appendFile('todo_list.txt', list, (err) => {
+                fs.appendFile(fileName, list, (err) => {
                     if (err) {console.err(err)} 
                     else {menu()}
                 })
@@ -56,12 +55,12 @@ const menu = () => {
         
         else if (response.toLowerCase()[0] === 'c') {
             completeResult = '';
-            for (let tasks of splitTaskInArr) {
+            for (let tasks of fs.readFileSync(fileName).toString().split('\n')) {
                 let taskSplit = tasks.split(" ");
                 if (taskSplit[0] === response.slice(1)) {
-                    let indexOfSelected = splitTaskInArr.indexOf(tasks)
+                    let indexOfSelected = fs.readFileSync(fileName).toString().split('\n').indexOf(tasks)
                     completeResult += `${taskSplit[0]} [*] ${taskSplit[3]}\n`;
-                    splitTaskInArr.splice(indexOfSelected, 1, completeResult);
+                    fs.readFileSync(fileName).toString().split('\n').splice(indexOfSelected, 1, completeResult);
                    console.log(`\ncompleted ${taskSplit[3]}\n`)
                 } else {
                     completeResult += `${tasks}\n`
@@ -73,16 +72,16 @@ const menu = () => {
         
         else if (response.toLowerCase()[0] === 'd') {
             deleteResult = '';
-            for (let tasks of splitTaskInArr) {
+            for (let tasks of fs.readFileSync(fileName).toString().split('\n')) {
                 let taskSplit = tasks.split(" ");
                 if (taskSplit[0] === response.slice(1)) {
-                    let indexOfSelected = splitTaskInArr.indexOf(tasks)
+                    let indexOfSelected = fs.readFileSync(fileName).toString().split('\n').indexOf(tasks)
                     if (taskSplit[1] === '[*]') {
                     console.log(`\ndeleted ${taskSplit[2]}\n`)                   
                     } else if (taskSplit[1] === '['){
                         console.log(`\ndeleted ${taskSplit[3]}\n`)
                     }
-                    splitTaskInArr.splice(indexOfSelected, 0);
+                    fs.readFileSync(fileName).toString().split('\n').splice(indexOfSelected, 0);
                 } else {
                     deleteResult += `${tasks}\n`
                 }
