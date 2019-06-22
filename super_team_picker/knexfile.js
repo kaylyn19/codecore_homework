047bfd74659e0cb3742aa32a4c4ce32a9bd16cc5
Kaylyn Kwak <kkwak0803@gmail.com>
2019-06-21 22:35:15 -0700

    initial commit with set-ups before starting the project

---
+++ super_team_picker/knexfile.js
@@ -0,0 +1,49 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'super_team_picker',
+      username: 'kaylyn',
+      password: '1234'
+    },
+    migrations: {
+      directory: './db/migrations'
+    }
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
