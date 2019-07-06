a6e1ba0d1e88f7aa125b6f8cf48059fa4da29a6d
Kaylyn Kwak <kkwak0803@gmail.com>
2019-07-06 15:21:49 -0700

    complete GET request on posts/new

---
+++ blog_on_rails/app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+    def new
+        @post = Post.new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def index
+    end
+
+    def destroy
+    end
+
+    def edit
+    end
+end
