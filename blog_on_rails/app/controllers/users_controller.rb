6771ee77d81341d7b72273d9c74107adcaa9c579
Kaylyn Kwak <kkwak0803@gmail.com>
2019-07-18 21:02:34 -0700

    change password with validations

--- blog_on_rails/app/controllers/users_controller.rb
+++ blog_on_rails/app/controllers/users_controller.rb
@@ -13,19 +13,41 @@ class UsersController < ApplicationController
         end
     end
 
-    def edit
+    def edit # for name and email
         @user = User.find_by(id: current_user.id)
     end
 
-    def update
+    def edit_password
         @user = User.find_by(id: current_user.id)
-        if @user.update params.require(:user).permit(:name, :email)
-            redirect_to posts_path, notice: 'User Profile Successfully Updated'
-        else
-            render :edit, alert: 'Try Again!'
+    end
+
+
+    def update # name and email
+        @user = User.find_by(id: current_user.id)
+        if params[:user][:current_password].blank? #editing user profile
+            if @user.update user_params 
+                redirect_to posts_path, notice: 'User Profile Successfully Updated'
+            else
+                render :edit, alert: "Try Again!"
+            end
+        else    # changing password
+            if @user&.authenticate(params[:user][:current_password]) == false#@user.password != params[:user][:current_password]
+                @user.errors.add(:current_password, "is different!")
+                return render :edit_password
+            
+            elsif params[:user][:password] == params[:user][:current_password]
+                @user.errors.add(:new_password, "can't be the same as the old password!")
+                return render :edit_password
+            end
+
+            if @user.update user_params 
+                redirect_to posts_path, notice: 'User Profile Successfully Updated'
+            else
+                render :edit_password, alert: "Try Again!"
+            end
         end
     end
-    
+
     private
 
     def user_params
