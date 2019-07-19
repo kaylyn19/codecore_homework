d166250d829fcc40d168c7ae7825232d0982ed59
Kaylyn Kwak <kkwak0803@gmail.com>
2019-07-18 21:12:03 -0700

    add admin user

--- blog_on_rails/config/routes.rb
+++ blog_on_rails/config/routes.rb
@@ -10,11 +10,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create, :edit, :update]
-  # resource :users do
-  #   resource :password, only: [:edit, :update]
-  # end
   get '/users/:id/edit_password', {to: 'users#edit_password', as: "edit_password"}
-  patch '/users/:id/update_password', {to: 'users#update_password', as: "password"}
 
   resource :session, only: [:new, :create, :destroy]
 end
