5f4770de10d6f1f50fbd9ec5c9e3e8764344d280
Kaylyn Kwak <kkwak0803@gmail.com>
2019-08-07 09:58:39 -0700

    border-bottom in the guess area

--- hangman/main.js
+++ hangman/main.js
@@ -1,3 +1,20 @@
 $(document).ready(() => {
+    const words = ['artichoke', 'eggplant', 'asparagus', 'broccoli', 'cabbage', 'mushroom', 'beans', 'carrots', 'onion', 'potato'];
     
-}
+    const index = Math.floor(Math.random() * words.length)
+    const randomWord = words[index]
+    const splitWord = randomWord.split('')
+    let correct = 0;
+    let wrong = 0;
+    let gameOver = false;
+
+    // add border-bottom for each letter of the word
+    for (let i = 0; i < randomWord.length; i++) {
+        $(".guess").append($('<div class="guessLine"></div>'))
+    }
+
+    // gamed ends
+    if (wrong >= 6) {
+        window.alert(`You lose! The answer was ${randomWord}`)
+    }
+})
